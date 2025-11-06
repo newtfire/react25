de3d154ba9fa2b13f98eaf39275f4b6e31e2ba2d
ebeshero <ebbondar@gmail.com>
2025-11-06 09:07:21 -0500

    reactive newt introduced

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
 import './App.css'
+import Newt from './components/Newt/Newt.jsx'
+
+/* The plan:
+
+Rotate the image on click by a varying number of degrees based
+on state (number of clicks).
+
+Same state should control other effects on the page, like information or images
+revealed.
+ */
 
 function App() {
 
   return (
     <>
-        <h1>Hello world!</h1>
 
+        <Newt />
     </>
   )
 }
---
+++ src/components/Newt/Newt.jsx
@@ -0,0 +1,12 @@
+import newt from "../../assets/newt-mosaic4.png";
+
+const homeNewt = () => {
+    return (
+        <figure className='homeNewt'>
+            <img src={newt} alt={"firebelly newt home icon"}/>
+            <figcaption>Clickable Newt</figcaption>
+        </figure>
+    );
+};
+
+export default homeNewt;
